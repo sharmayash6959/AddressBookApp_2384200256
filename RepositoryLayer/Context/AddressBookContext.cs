010ee19e42131ce5075b62d4e23a22e8a87afb1a
Yash Sharma <sharmayash6959@gmail.com>
2025-03-13 22:04:56 +0530

    [Yash Sharma] Add: Added code for UC 1:Configure Database and Application Settings of section 1 into branch

---
+++ RepositoryLayer/Context/AddressBookContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entity;
+
+namespace RepositoryLayer.Context
+{
+    public class AddressBookContext : DbContext
+    {
+        public AddressBookContext(DbContextOptions<AddressBookContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AddressBook> AddressBooks { get; set; }
+    }
+}
