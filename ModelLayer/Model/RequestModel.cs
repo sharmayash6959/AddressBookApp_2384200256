c37d7103e9e3fea5c94e486288ef6cb22b5e1c0d
Yash Sharma <sharmayash6959@gmail.com>
2025-03-14 19:11:27 +0530

    [Yash Sharma] Add : Code for UC2: Implement Address Book API Controller

---
+++ ModelLayer/Model/RequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model
+{
+    public class RequestModel
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+    }
+}
